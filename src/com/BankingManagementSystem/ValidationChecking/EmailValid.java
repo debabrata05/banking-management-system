eb7949009beffff2e546e65fc6c9795a2363cdd8
debabrata05 <debabratakrkaran@gmail.com>
2017-10-24 00:02:34 +0530

    all done

---
+++ src/com/BankingManagementSystem/ValidationChecking/EmailValid.java
@@ -0,0 +1,41 @@
+package com.BankingManagementSystem.ValidationChecking;
+import java.util.ArrayList;
+
+import com.BankingManagementSystem.FileHandling.*;
+import com.BankingManagementSystem.frameDesign.*;
+import com.BankingManagementSystem.Pojo.*;
+
+public class EmailValid {
+	int accNo;
+	String a;
+	ArrayList<CustomerDetails> userlist = CustomerDetailsFile.readDataFromFile();	
+	
+	public void Email(String msg,String accno) 
+	{
+		
+		a=userlist.get(Search.searchId(accno)).getCemailid();
+		
+		String[] to={a};
+	try{	
+		if(EmailSender.sendMail("imduke021@gmail.com", "bms@12121121",msg,to))
+		{
+			System.out.println("email sent");
+		}
+		else
+			System.out.println("error occured");
+	 }catch (Exception e) {
+		System.out.println(e);
+	}
+	}
+	
+	
+	/*
+	public static void main(String[] args){
+		EmailValid obj=new EmailValid();
+		
+		//obj.main(); //passing account number
+	}
+	*/
+	
+
+}
